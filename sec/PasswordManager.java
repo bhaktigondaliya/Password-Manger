2dd72de3584bd071f4af69f50f37125400ff56ad
Gondaliya Bhakti <bgondaliya957@gmal.com>
2026-04-01 21:45:10 +0530

    JFrame created

---
+++ sec/PasswordManager.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+//import java.awt.event.*;
+
+public class PasswordManager {
+
+    public static void main(String a[]){
+        JFrame fr = new JFrame("Password Manager");
+        fr.setSize(500,400);
+        fr.setVisible(true);
+        fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    
+    
+
+    
+    
+    
+}
